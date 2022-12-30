248a7df3cb651b081f9b9cb17010cebab2629608
shoxrux0607 <chullievshoxrux0607@gmail.com>
2022-12-31 04:03:33 +0500

    001

--- build_func01.py
+++ build_func01.py
@@ -1,5 +1,6 @@
 def main(number):
-    """A integer type variable 'number' is given. Return the absolute value of a "number". 
+    """A integer type variable 'number' is given.
+     Return the absolute value of a "number". 
 
     Args:
         number (int): integer
@@ -7,6 +8,9 @@ def main(number):
     Returns:
         int: the absolute value of a "number"
     """
+
     x = abs(number)
     return x
-print(main(-8))
+
+
+print(main(-18))
